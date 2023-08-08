c70b365abecc0a46585c457293ebb45d25b091de
Leandro <leandrolopez131982@gmail.com>
2023-08-07 22:12:34 -0300

    import de potencia y peerdepenencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {potencia, Greetings} from 'poc-lib-shared-pow';
+/* import {Greetings}from 'poc-lib-shared-pow'; */
 
 function App() {
+
+  let resultado = potencia(5, 4);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <Greetings />
+     <h2>{resultado}</h2>
+      
     </div>
   );
 }
