4969b71c6dbf8e6ab3bdd43c8eaea8ba2da2d3f0
Leandro <leandrolopez131982@gmail.com>
2023-08-17 12:24:46 -0300

    instalacion de mantine

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import {potencia, Greetings} from 'poc-lib-shared-pow';
+import { Mantine } from '@mantine/core';
 
 function App() {
 
-  let resultado = potencia(3, 3);
+  let resultado = potencia(2, 4);
+  console.log(Greetings);
   return (
     <div className="App">
 
      <Greetings />
+
      <h2>{resultado}</h2>
-      
+
     </div>
   );
 }
-console.log("hola")
 
 export default App;
